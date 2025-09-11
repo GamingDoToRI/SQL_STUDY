b30a2b6aafc5b86752df42bfc6aa06255788deef
GamingDoToRI <jmj000418@gmail.com>
2025-09-11 23:03:52 +0900

    09_11

---
+++ HackerRank_Easy/Draw The Triangle 2.sql
@@ -0,0 +1,12 @@
+WITH RECURSIVE
+    pattern AS (
+        SELECT 1 AS n
+        UNION ALL
+        SELECT n + 1
+        FROM pattern
+        WHERE n < 20
+        )
+SELECT
+    REPEAT('* ', n) AS stars
+FROM
+    pattern;
