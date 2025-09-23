6e3725f6d471d3374acd6aeef410a8a31c5e0fb8
GamingDoToRI <jmj000418@gmail.com>
2025-09-23 15:25:07 +0900

    09_23

---
+++ Leetcode_Easy/Patients With a Condition.sql
@@ -0,0 +1,7 @@
+SELECT
+    *
+FROM
+    patients
+WHERE
+    conditions LIKE 'DIAB1%' 
+    OR conditions LIKE '% DIAB1%';
---
+++ Leetcode_Easy/Top Travellers.sql
@@ -0,0 +1,13 @@
+SELECT
+    name,
+    IFNULL(SUM(distance), 0) AS travelled_distance
+FROM
+    users u
+    LEFT JOIN rides r
+    ON u.id = r.user_id
+GROUP BY
+    u.id,
+    name
+ORDER BY
+    2 DESC,
+    1;
